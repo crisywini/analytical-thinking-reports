ccba5b5d6ac80326d00af25ed425b580d7c8d7f5
Cristian Sanchez Pineda <christian.sanchez@kinesso.com>
2022-10-07 23:32:33 -0500

    Added template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Here we made fun stuffs, classifications, clustering, and rules',
+    author='Cristian Giovanny Sánchez Pineda',
+    license='MIT',
+)
